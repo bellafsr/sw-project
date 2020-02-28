da884cd8f478a10fcc8d6d1a7bacad713b311935
Isabella Rodrigues <iferreira_prest@globosat.com.br>
2020-02-28 15:44:22 -0300

    first Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <card :planet="planet"/>
+    <button class="btn-next" @click="getRandomPlanet">Next</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import card from './components/card.vue';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{card},
+  data(){
+    return{
+      planet: {},
+    }
+  },
+  methods:{
+    getRandomPlanet(){
+      let planetNumber = Math.floor(Math.random() * (61 - 1)) + 1;
+      this.$http.get('/planets/'+planetNumber).then(res => {
+        this.planet = 
+        { 
+          name: res.data.name, 
+          population: res.data.population,
+          climate: res.data.climate, 
+          terrain: res.data.terrain, 
+          featured: (res.data.films).length
+        }
+      })
+    }
+  },
+  created(){
+    this.getRandomPlanet()
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,body{
+  margin:0;
+  padding:0;
+  height: 100%;
+  width: 100%;
+  font-family: 'Open Sans';
 }
+
+#app{
+  height: inherit;
+  background: repeat url('https://unblast.com/wp-content/uploads/2018/10/Sky-Stars-Pattern.jpg');
+  background-size: 500px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+#app .btn-next{
+  padding:15px 50px;
+  border: 3px solid #fff;
+  background-color:transparent;
+  border-radius:10px;
+  margin-top:20px;
+  color:#fff;
+  font-size:20px;
+  font-weight: bold;
+  text-transform: uppercase;
+  cursor: pointer;
+  transition: 0.3s;
+}
+
+#app .btn-next:focus{outline: 0}
+
+#app .btn-next:hover{
+  background-color:#fff;
+  color:#000;
+  transition: 0.3s;
+}
+
 </style>
---
+++ src/components/card.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="card">
+    <h1>{{planet.name}}</h1>
+    <hr>
+    <div class="generalInfo">
+      <h2>Population: {{planet.population}}</h2>
+      <h2>Climate: {{planet.climate}}</h2>
+      <h2>Terrain: {{planet.terrain}}</h2>
+    </div>
+    <h4>Featured in {{planet.featured}} films </h4>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{planet: Object}
+}
+</script>
+
+<style scoped>
+
+.card{
+  width: 25%;
+  height: 72%;
+  min-height:600px; 
+  border: 5px solid #DDC706;
+  background-color: rgba(0, 0, 0, 0.61);
+  color: #DDC706;
+  display: flex;
+  flex-direction: column;
+}
+.card h1{
+  font-size:2.5rem;
+}
+.card .generalInfo h2{
+  font-size:1.5rem
+}
+
+.card h4{
+  flex: 1 1 auto;
+  font-size:1rem;
+  margin: 0px;
+  padding:0px;
+}
+.card h1, 
+.card h4{
+  text-align: center;
+}
+
+.card hr{
+  background-color:#DDC706;
+  border: none;
+  height: 5px;
+  margin:0px;
+}
+.card .generalInfo{
+  flex: 2 1 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 0 10%;
+}
+
+</style>
